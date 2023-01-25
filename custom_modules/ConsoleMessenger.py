312b02eb27193c0c5c1ae88fe3915619b424f87b
xuoxod <xuoxod@gmail.com>
2023-01-25 01:02:35 -0500

    Initial commit

---
+++ custom_modules/ConsoleMessenger.py
@@ -0,0 +1,29 @@
+import colorama
+from colorama import Fore, Back, Style
+
+colorama.init(autoreset=True)
+
+
+def error(arg=""):
+    return "{}{}{}".format(Fore.RED, Style.BRIGHT, arg)
+
+
+def warning(arg=""):
+    return "{}{}{}".format(Fore.YELLOW, Style.BRIGHT, arg)
+
+
+def success(arg=""):
+    return "{}{}{}".format(Fore.GREEN, Style.BRIGHT, arg)
+
+
+def custom(r, g, b, text):
+    return "\033[38;2;{};{};{}m{} \033[38;1;255;255;255m".format(r, g, b, text)
+
+
+CONSOLE_MESSENGER_SWITCH = {
+    "error": error,
+    "warning": warning,
+    "success": success,
+    "custom": custom,
+    "reset": lambda: Style.RESET_ALL,
+}
